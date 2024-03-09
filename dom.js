21b8bb7975d6f44d2d99f25cbe848094f70ae31e
ridwaanibrahim <mukhtaribrahim483@gmail.com>
2024-03-09 20:17:23 +0300

    first commit

---
+++ dom.js
@@ -0,0 +1,6 @@
+//getelementbyclassname() length
+var data=document.getElementsByClassName("geek");
+var len=data.length;
+document.write(len +"br");
+document.write(data +"br");
+console.log(data);
